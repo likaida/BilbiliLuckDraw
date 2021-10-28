897618c06a3f5f7d42ac6cca2081ab3a24947936
likaida <1264626829@qq.com>
2021-10-28 09:24:45 +0800

    check

--- app/src/main/java/com/aceli/bilibililuckdraw/LuckDrawApplication.kt
+++ app/src/main/java/com/aceli/bilibililuckdraw/LuckDrawApplication.kt
@@ -14,7 +14,7 @@ import timber.log.Timber
 
 
 class LuckDrawApplication : Application() {
-    // No need to cancel this scope as it'll be torn down with the process
+    // No need to cancel this scope as it'll be torn down with the process //
     val applicationScope = CoroutineScope(SupervisorJob())
     // Using by lazy so the database and the repository are only created when they're needed
     // rather than when the application starts
